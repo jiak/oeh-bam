65c320ffd8765afffc812331bdba4a716e83cdf3
mo <mo@lightapps.com.au>
2016-02-12 14:44:09 +1100

    basic structure stuff

--- js/Controllers/VegetationController.js
+++ js/Controllers/VegetationController.js
@@ -1,16 +1,42 @@
 angular.module('BAMApp.controllers').controller('VegetationController', function ($scope, dataService) {
+
+    this.pctPane = {
+        model: {
+            referenceData: dataService.referenceData,
+            pctInput: {
+                input: {
+                    pct: [],
+                    vegetationZones: [],
+                    futureVegetationZones: []
+                }
+            }
+
+        },
+
+        createPctObject: function() {
+            return {
+                "formation": null,
+                "keithClass": null,
+                "pct": null,
+                "tec": null,
+                "percentCleared": "waiting",
+                "threatStatusClass": "waiting"
+            }
+        }
+    }
+
     $scope.referenceData = dataService.referenceData;
-	$scope.input = dataService.input;
-	$scope.anotherVegetation = function () {
-		$scope.input.vegetation.push(			{
-				"formation": null,
-				"keithClass": null,
-				"pct": null,
-				"tec": null,
-				"percentCleared": "waiting",
-				"threatStatusClass": "waiting"
-			});
-	};
+    $scope.input = dataService.input;
+    $scope.anotherVegetation = function () {
+        $scope.input.vegetation.push({
+            "formation": null,
+            "keithClass": null,
+            "pct": null,
+            "tec": null,
+            "percentCleared": "waiting",
+            "threatStatusClass": "waiting"
+        });
+    };
 });
 
 
