92dc695d6b1a51f4263f33ee35bda2ce67a58e4e
mo <mo@lightapps.com.au>
2016-02-12 16:38:13 +1100

    Built out remaining panes in vegetation tab

--- js/Controllers/VegetationController.js
+++ js/Controllers/VegetationController.js
@@ -22,8 +22,43 @@ angular.module('BAMApp.controllers').controller('VegetationController', ["$scope
             }
         },
 
+        createVegetationZoneItem: function () {
+            return {
+                pctCode: null,
+                conditionClass: null,
+                identifier: null,
+                area: null,
+                composition: null,
+                structure: null,
+                function: null,
+                vis: null
+            }
+        },
+
+        createFutureVegetationZoneItem: function () {
+            return {
+                typeCode: null,
+                conditionClass: null,
+                identifier: null,
+                area: null,
+                composition: null,
+                structure: null,
+                function: null,
+                vis: null,
+                changeVis: null
+            }
+        },
+
         addPctObject: function () {
             this.model.input.pct.push(this.createPctObject())
+        },
+
+        addVegetationZoneItem: function () {
+            this.model.input.vegetationZones.push(this.createVegetationZoneItem())
+        },
+
+        addFutureVegetationZoneItem: function() {
+            this.model.input.futureVegetationZones.push(this.createFutureVegetationZoneItem())
         }
     }
 }]);
