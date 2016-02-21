3f0212da0800c764e7c426bc4d1f4beb316d3f9f
baowenlu <baowen.lu@gmail.com>
2016-02-22 10:08:59 +1100

    Add habitat tab

---
+++ js/Controllers/HabitatController.js
@@ -0,0 +1,5 @@
+angular.module('bamApp').controller('habitatController', ["$scope", "referenceDataService", "$rootScope", "dataService",  function ($scope, referenceDataService, $rootScope, dataService) {
+
+	$scope.model = dataService.habitatModel;
+	
+}]);
