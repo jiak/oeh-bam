bbf38c9459330d37a2cc88a575d850a0959c77d5
baowenlu <baowen.lu@gmail.com>
2016-02-22 17:36:05 +1100

    fixed bugs

--- js/Controllers/HabitatController.js
+++ js/Controllers/HabitatController.js
@@ -1,8 +1,8 @@
 angular.module('bamApp').controller('habitatController', ["$scope", "referenceDataService", "$rootScope", "dataService",  function ($scope, referenceDataService, $rootScope, dataService) {
 
 	$scope = {
-		model: dataService.habitatModel;
-		
+		model: dataService.habitatModel
+
 	}
 
 }]);
